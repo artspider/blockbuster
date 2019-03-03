22d44ce06cbc54ec6d6b7d7ffc36ab827d37101f
artspider <arturo.itiguala@gmail.com>
2019-03-03 09:33:15 -0600

    Se creo el modelo y los seeders con fakerFactory

--- app/alumno.php
+++ app/alumno.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class alumno extends Model
+class Alumno extends Model
 {
-    //
+    //indicamos con que tabla se relaciona
+    protected $table = 'alumno';
+    //indicamos los campos que podemos llenar
+    protected $fillable = ['nombre', 'email', 'password', ];
 }
---
+++ database/seeds/AlumnoSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class AlumnoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for($i=0; $i < 50; $i++){
+            DB::table('alumno')->insert(array(
+                'nombre' => $faker->firstNameMale,
+                'email' => $faker->email,
+                'password' => $faker->randomDigit,
+                'created_at' => date('Y-m-d H:m:s'),
+                'updated_at' => date('Y-m-d H:m:s')
+            ));
+        }
+        /*
+        DB::table('alumno')->insert([
+            'nombre' => 'Pedro Morales',
+            'email' => 'pmmail@gmail.com',
+            'password' => 'micontraseña',
+        ]);
+        */
+    }
+}
