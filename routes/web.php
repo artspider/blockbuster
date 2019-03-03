2829699ec637511e347ddf23f21cda1ab94f4b71
artspider <arturo.itiguala@gmail.com>
2019-03-02 19:06:18 -0600

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('vista', function () {
+    return view('vista');
+});
+
+Route::resource('libros', 'librosController');
+
+Route::get('libros/{genero}', function ($genero) {
+    switch ($genero) {
+        case 'amor':
+            return "Libros de amor";
+            break;
+        case 'drama':
+            return "Libros de drama";
+            break;
+        case 'terror':
+            return "Libros de terror";
+            break;
+        case 'aventura':
+            return "Libros de aventura";
+            break;
+
+        default:
+            return "No existe esa categoria";
+            break;
+    }
+});
